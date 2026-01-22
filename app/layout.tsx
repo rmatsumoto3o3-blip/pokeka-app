2b19c4ff0de24689079fecd0128ab8c55d07e01e
rmatsumoto3o3-blip <r.matsumoto.3o3@gmail.com>
2026-01-22 15:52:06 +0900

    feat: Add Google Analytics (GA4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono, Press_Start_2P } from "next/font/google"; // Import pixel font
 import "./globals.css";
 import AdSenseLoader from "@/components/AdSenseLoader";
@@ -35,6 +36,20 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} ${pressStart2P.variable} antialiased`}
       >
         <AdSenseLoader />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-2JW6DYQYSD"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-2JW6DYQYSD');
+          `}
+        </Script>
         {children}
       </body>
     </html>
